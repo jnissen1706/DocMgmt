f846c6b78899a65068255815743509e638eb1693
Josh Nissen <josh.nissen@coxinc.com>
2022-10-28 17:20:47 -0500

    Added startGraphClient function & graphClient prop

--- src/webparts/docMgmt/DocMgmtWebPart.ts
+++ src/webparts/docMgmt/DocMgmtWebPart.ts
@@ -12,6 +12,7 @@ import * as strings from 'DocMgmtWebPartStrings';
 import DocMgmt from './components/DocMgmt';
 import { IDocMgmtProps } from './components/IDocMgmtProps';
 import { getSP } from './pnpjsConfig';
+import { MSGraphClientV3 } from "@microsoft/sp-http";
 
 export interface IDocMgmtWebPartProps {
   description: string;
@@ -21,6 +22,7 @@ export default class DocMgmtWebPart extends BaseClientSideWebPart<IDocMgmtWebPar
 
   private _isDarkTheme: boolean = false;
   private _environmentMessage: string = '';
+  private _graphClient: MSGraphClientV3 = null;
 
   public render(): void {
     const element: React.ReactElement<IDocMgmtProps> = React.createElement(
@@ -29,9 +31,12 @@ export default class DocMgmtWebPart extends BaseClientSideWebPart<IDocMgmtWebPar
         context: this.context,
         description: this.properties.description,
         isDarkTheme: this._isDarkTheme,
-        environmentMessage: this._environmentMessage
+        environmentMessage: this._environmentMessage,
+        graphClient: this._graphClient
       }
     );
+
+    let fakeElement = this.startGraphClient(element);
 
     ReactDom.render(element, this.domElement);
   }
@@ -44,7 +49,7 @@ export default class DocMgmtWebPart extends BaseClientSideWebPart<IDocMgmtWebPar
     //Initialize our _sp object that we can then use in other packages without having to pass around the context.
     //  Check out pnpjsConfig.ts for an example of a project setup file.
     getSP(this.context);
-  }
+}
 
   private _getEnvironmentMessage(): string {
     if (!!this.context.sdks.microsoftTeams) { // running in Teams
@@ -101,4 +106,36 @@ export default class DocMgmtWebPart extends BaseClientSideWebPart<IDocMgmtWebPar
       ]
     };
   }
+
+  //For Graph Client initialization. Currently not functioning correctly and returns a null element.
+  protected startGraphClient(InElement?:React.ReactElement<IDocMgmtProps>):React.ReactElement<IDocMgmtProps> {
+    try {
+        const context1 = InElement.props.context;
+        let element: React.ReactElement<IDocMgmtProps> = null;
+        const props: Promise<React.ReactElement<IDocMgmtProps>> = new Promise<React.ReactElement<IDocMgmtProps>>(
+            (resolve, reject) => {
+                context1.msGraphClientFactory
+            .getClient('3')
+            .then((client: MSGraphClientV3): void => {
+            const element1: React.ReactElement<IDocMgmtProps> = React.createElement(DocMgmt,
+            {
+                context: context1,
+                description: InElement.props.description,
+                isDarkTheme: InElement.props.isDarkTheme,
+                environmentMessage: InElement.props.environmentMessage,
+                graphClient: client
+                });
+            element = element1;
+            }).catch((errorL) => {
+              console.log(``);
+        });
+    });
+
+    return element;
+}
+    catch(ex) {
+        console.log(`Error`)
+    }
+}
+
 }
--- src/webparts/docMgmt/components/IDocMgmtProps.ts
+++ src/webparts/docMgmt/components/IDocMgmtProps.ts
@@ -1,8 +1,9 @@
 import { WebPartContext } from "@microsoft/sp-webpart-base";
-
+import { MSGraphClientV3 } from "@microsoft/sp-http";
 export interface IDocMgmtProps {
   context: WebPartContext;
   description: string;
   isDarkTheme: boolean;
   environmentMessage: string;
+  graphClient: MSGraphClientV3;
 }
